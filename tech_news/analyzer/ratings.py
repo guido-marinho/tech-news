51d3e3fa5f03686df18826e09073dbf34f1fcbcb
guilhermegattimarinho <ggattimarinho@gmail.com>
2024-03-12 17:47:49 -0300

    ✨feat:  Implementa função top_5_categories() que retorna as 5 categorias mais comuns

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,10 @@
-# Requisito 10
+from tech_news.database import find_news
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = find_news()
+    categories = sorted([item["category"] for item in news])
+    counter = Counter(categories)
+    most_common = counter.most_common(5)
+    return [category for category, count in most_common]
